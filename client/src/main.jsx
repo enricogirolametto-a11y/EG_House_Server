dba4c91e236f4e05a7ee1617f6016ecec4acfa52
EG-DesignEvo <enricogirolametto@gmail.com>
2025-12-24 19:52:59 +0100

    maio

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+console.log("Chiave Clerk " ,PUBLISHABLE_KEY)
+
 if (!PUBLISHABLE_KEY) {
   throw new Error("Chiave Clerk mancante nel file .env")
 }
@@ -14,6 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
-    </ClerkProvider>
+    </ClerkProvider>S
   </React.StrictMode>
 )
